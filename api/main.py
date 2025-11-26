fd29bf28590ee372c1c4c1479a7feb4c1a535c62
josef-karpinski <josef.karpinski@uconn.edu>
2025-11-25 19:18:46 -0500

    feat: predict endpoint inputs

--- api/main.py
+++ api/main.py
@@ -1,7 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Query
+from enum import Enum
+from datetime import date
 
 app = FastAPI()
 
+class MealTime(str, Enum):
+    breakfast = "breakfast"
+    lunch = "lunch"
+    dinner = "dinner"
+
 @app.post("/predict")
-def predict():
-    return {"message": "Hello from the endpoint"}
+async def predict(
+    request: Request,
+    dining_hall_id: str = Query(...),
+    meal_time: MealTime = Query(...),
+    meal_date: date = Query(...)
+):
+    file_bytes = await request.body()
+    return {
+        "message": "Hello from the endpoint",
+        "meal_time": meal_time,
+        "dining_hall_id": dining_hall_id,
+        "date": meal_date,
+        "file_size": len(file_bytes)
+    }
